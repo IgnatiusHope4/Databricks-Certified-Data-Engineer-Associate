0f616ce0c491328abfe2820d3aed1d62f4719c6e
IgnatiusHope4 <IgnatiusHope4>
2023-08-30 04:08:48 +0000

    Adding New Notebooks

---
+++ My Folder/(Clone) Notebook basics 2023-08-26 18:44:40.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+print ("Hello world")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "hello world from SQl"
+
+# COMMAND ----------
+
+# MAGIC %run /Includes/Setup
+
+# COMMAND ----------
+
+print(Full_name)
+
+# COMMAND ----------
+
+
